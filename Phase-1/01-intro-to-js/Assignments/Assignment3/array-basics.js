af6d0cdfe4a10252b635b8d12870f77934860b37
mohdhaseeb7 <haseeb20057@gmail.com>
2026-01-26 13:30:31 +0000

    Assignment3

---
+++ Phase-1/01-intro-to-js/Assignments/Assignment3/array-basics.js
@@ -0,0 +1,11 @@
+let myInfo =[ "Haseeb", 20 , 'Hyderabad' , true]
+console.log(myInfo[1])
+myInfo[2] = "Bangalore"
+console.log(myInfo)
+
+
+let colors= [ 'red' , 'yellow' , "blue"]
+colors.push('orange','violet')
+console.log(colors)
+
+let numbers = 
