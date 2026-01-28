18c576e23816a96015f90765da68816fb2e653c2
mohdhaseeb7 <haseeb20057@gmail.com>
2026-01-28 16:13:09 +0000

    Assignment4done

---
+++ Phase-1/01-intro-to-js/Assignments/Assignment4/2section.js
@@ -0,0 +1,98 @@
+//Question11
+let age = 15 
+if(age >= 18) {
+    console.log('Adult')
+}
+else{
+    console.log('Minor')
+}
+
+//Question12 Prediction: Number is less than 10
+let num = 7
+
+if (num >= 10) {
+    console.log('Number is greater than or equal to 10')
+} else {
+    console.log('Number is less than 10')
+}
+
+
+//Question13
+let number=22
+if(number%2==0){
+    console.log('Even')
+}
+else{
+    console.log('Odd')
+}
+
+//Question14 Predction: Hello user
+let username = 'omer'
+
+if (username == 'admin') {
+    console.log('Hello admin')
+} else {
+    console.log('Hello user')//Hello user
+}
+
+//Question15
+let password = 'pass123'
+if (password == 'admin123'){
+    console.log('Login successful')
+}
+else{
+    console.log('Wrong password')
+}
+
+//Question16 Prediction: Fail
+let marks = 35
+
+if (marks >= 40) {
+    console.log('Pass')
+} else {
+    console.log('Fail') //Fail
+}
+
+//Question17
+let a = 10
+let b = 20
+if(a>b){
+    console.log('a is greater than b')
+}
+else{
+    console.log('b is greater than a')
+    
+}
+
+//Question18 Prediction: Pleasent weather
+let temperature = 25
+
+if (temperature > 30) {
+    console.log('Hot weather')
+} else {
+    console.log('Pleasant weather')
+}
+
+
+//Question19
+let numm = -5
+if (numm < 0){
+    console.log('the number is negative')
+}
+else {
+    console.log('number is positive')
+}
+
+//Question20  Prediction: Ahmed  Omer
+let name = 'Omer'
+
+if (true) {
+    let name = 'Ahmed'
+    console.log(name)
+}
+
+console.log(name)
+//Explanation: because the variable works inside the scope
+//we can't use the variable outside
+
+
---
+++ Phase-1/01-intro-to-js/Assignments/Assignment4/5section.js
@@ -0,0 +1,41 @@
+//Question39 Predicted: Max value is : 50
+let a = 20
+let b = 50
+let max
+
+if (a > b) {
+    max = a
+} else {
+    max = b
+}
+
+console.log(`Max value is: ${max}`)
+
+
+//Question40
+let namee = 'Omer'
+let agge = 25
+console.log(`My name is ${namee} and I am ${agge} years old`)
+
+
+
+//Question41 Prediction: Price:5000 , Discount: 20%, Final:4000
+let price = 5000
+let discount = 20
+
+console.log(`Price: ${price}, Discount: ${discount}%, Final: ${price - (price * discount / 100)}`)
+
+
+//Question42
+let item = 'Laptop'
+let pricee = 50000
+let quantity = 2
+console.log(`Item:${item},Price:${pricee},Quantity:${quantity},Total:${pricee*quantity}`)
+
+//Question43
+// Method 1
+// console.log('Max value is: ' + max)
+
+// Method 2
+// console.log(`Max value is: ${max}`)
+//The difference is that in method 1 we are using simple string and in method 2 we are using string literal
